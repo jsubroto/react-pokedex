0358c58a20e58189f22b762ff9204470f87b8681
Jaimes Subroto <jaimessubroto@gmail.com>
2022-10-09 16:16:36 -0700

    Get individual pokemon data

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { PokemonProps } from "../types";
 
 export const Pokemon = ({
 	name,
 	url
 }: PokemonProps) => {
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+			.then(res => res.json())
+			.then(data => console.log(data))
+	}, [name])
+
 	return (
 		<li>{name}</li>
 	)
