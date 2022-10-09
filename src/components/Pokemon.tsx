bdcec3eab40ae9bcf68593bbb62e3e984fcc7ce7
Jaimes Subroto <jaimessubroto@gmail.com>
2022-10-09 16:35:38 -0700

    Show the pokemon's id, image, and type(s)

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,17 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PokemonProps } from "../types";
+
+type PokemonData = {
+	id: number
+	imageUrl: string
+	types: { type: { name: string } }[]
+}
 
 export const Pokemon = ({
 	name,
 	url
 }: PokemonProps) => {
+	const [pokemonData, setPokemonData] = useState<PokemonData>()
+
 	useEffect(() => {
 		fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
 			.then(res => res.json())
-			.then(data => console.log(data))
+			.then(data => setPokemonData({
+				id: data.id,
+				imageUrl: data.sprites.front_default,
+				types: data.types
+			}))
 	}, [name])
 
 	return (
-		<li>{name}</li>
+		<li>{name} {pokemonData?.id}: {pokemonData?.types.map(type => type.type.name).join(', ')}</li>
 	)
 }
