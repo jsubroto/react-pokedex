e7dede3e739dd55e862014d0d68fed7b5930682c
Jaimes Subroto <jaimessubroto@gmail.com>
2022-10-09 23:13:07 -0700

    Add back and next button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,23 @@ import { PokemonProps } from './types'
 
 const App = () => {
   const [pokemons, setPokemons] = useState<PokemonProps[]>([])
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
+  const [previousUrl, setPreviousUrl] = useState('')
+	const [nextUrl, setNextUrl] = useState('')
 
   useEffect(() => {
     const getPokemons = async () => {
       try {
-        const data = await fetchJson('https://pokeapi.co/api/v2/pokemon/')
+        const data = await fetchJson(url)
         setPokemons(data.results)
+        setPreviousUrl(data.previous)
+        setNextUrl(data.next)
       } catch (e) {
         console.error(e)
       }
     }
     getPokemons()
-  }, [])
+  }, [url])
 
   const pokemonElements = pokemons.map(pkmn => 
     <Pokemon key={pkmn.name} name={pkmn.name} url={pkmn.url}/>
@@ -26,6 +31,8 @@ const App = () => {
   return (
     <div className='max-w-[86%] mx-auto'>
       <h1 className='text-2xl text-center my-4'>Pokédex</h1>
+      <button onClick={() => setUrl(previousUrl)}>Back</button>
+      <button onClick={() => setUrl(nextUrl)}>Next</button>
       <div className={responsiveGrid}>
         {pokemonElements}
       </div>
