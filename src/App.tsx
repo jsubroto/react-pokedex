58ad6315d7318246e6e989cadf394a5c36a09eaf
Jaimes Subroto <jaimessubroto@gmail.com>
2022-10-10 23:00:55 -0700

    Add dark/light mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Pagination, Pokemon } from './components/index'
 import { fetchJson } from './lib/fetch'
 import { responsiveGrid } from './styles'
+import { MoonSvg, SunSvg } from './svg/index'
 import { PokemonProps } from './types'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
   const [previousUrl, setPreviousUrl] = useState('')
 	const [nextUrl, setNextUrl] = useState('')
+  const [darkTheme, setDarkTheme] = useState(false)
 
   useEffect(() => {
     const getPokemons = async () => {
@@ -28,11 +30,25 @@ const App = () => {
     <Pokemon key={pkmn.name} name={pkmn.name} url={pkmn.url}/>
   )
 
-  const handlePagination = (url: string) => setUrl(url)
+  const toggleTheme = () => {
+    const root = window.document.documentElement
+    setDarkTheme(!darkTheme)
+    if (!darkTheme) root.classList.add('dark')
+    else root.classList.remove('dark')
+  }
 
+  const handlePagination = (url: string) => setUrl(url)
   return (
-    <div className='max-w-[86%] mx-auto'>
-      <h1 className='text-3xl text-center mt-4'>Pokédex</h1>
+    <div className='max-w-[86%] mx-auto dark:bg-black dark:text-white'>
+      <div className='flex justify-center pt-4'>
+        <h1 className='text-3xl text-center'>Pokédex</h1>
+        <button onClick={toggleTheme}>
+          {darkTheme 
+            ? <SunSvg />
+            : <MoonSvg />
+          }
+        </button>
+      </div>
       <Pagination 
         handleClick={handlePagination} 
         prevUrl={previousUrl} 
@@ -42,7 +58,7 @@ const App = () => {
         {pokemonElements}
       </div>
       <footer className='mx-auto border-t md:w-2/3'>
-        <p className='text-center my-2'>
+        <p className='text-center py-2'>
           Copyright (c) 2022 Jaimes Subroto
         </p>
       </footer>
